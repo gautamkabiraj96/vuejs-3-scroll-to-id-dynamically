ecec62cb3c7cd13e55329aad2dda09781ca359a2
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2022-12-02 00:56:58 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# vuejs-3-scroll-to-id-dynamically
-Created with CodeSandbox
+# Scroll to an element in Vue 3 | Example
+
+Learn how to scroll to a dynamically assigned Id element or any element with class in Vue 3 programmatically with a code example. [Learn more](https://www.nightprogrammer.com/vue-js/how-to-scroll-to-an-element-in-vue-3-example/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue';
+
+const targetId = ref('200')
+
+const scrollToElement = () =>  {
+  const el = document.getElementById(targetId.value);
+
+  if (el) {
+    el.scrollIntoView();
+  }
+}
+</script>
+
+<template>
+<div>
+    <button @click="scrollToElement">Scroll to {{targetId}}</button>
+    <input type="text" v-model="targetId" />
+    <ul>
+      <li v-for="n of 500" :key="n" :id="n">
+        {{ n }}
+      </li>
+    </ul>
+</div>
+</template>
+
+<style scoped>
+ul {
+  padding-lefT: 0;
+}
+li {
+  background: #eee !important;
+  list-style-type: none;
+  margin-bottom: 1em;
+  margin-top: 1em;
+  padding: 4px;
+}
+</style>
